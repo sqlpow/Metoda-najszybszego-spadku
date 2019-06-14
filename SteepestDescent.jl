406aa2b54a3348dbec646f77a6777038c266ee7d
sqlpow <49656874+sqlpow@users.noreply.github.com>
2019-06-14 13:44:15 +0200

    Added function to calculate gradient in algorithm

--- SteepestDescent.jl
+++ SteepestDescent.jl
@@ -1,22 +1,26 @@
-
-function SteepestDescent(df::Function, x::Integer)
+using ForwardDiff
+  
+   
+        function SteepestDescent(f::Function, x::Integer)
         
-    precision = 0.000001
-    max_iters = 10000
-    previous_step_size = 1
-    rate = 0.01 
-    iters = 0 
-         
-        
-    while previous_step_size > precision && iters < max_iters
-        prev_x = x 
-        x = x - rate * df(prev_x) 
-        previous_step_size = abs(x - prev_x) 
-        iters = iters+1 
-        println("Iteration ", iters)
-        println("x value is ", x)
-    end
-print("\nThe local minimum occurs at ", x)
-end
+            precision = 0.000001
+            max_iters = 10000
+            previous_step_size = 1
+            rate = 0.01 
+            iters = 0 
 
+
+            while previous_step_size > precision && iters < max_iters
+                prev_x = x 
+                df = ForwardDiff.gradient(z -> f(z[1]), [prev_x])
+                x = x - rate *  df[1]
+                previous_step_size = abs(x - prev_x) 
+                iters = iters+1 
+                println("Iteration ", iters)
+                println("x value is ", x)
+            end
+        print("\nThe local minimum occurs at ", x)
+        
+        end
+        
 
